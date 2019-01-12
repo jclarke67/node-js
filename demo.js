60a723aff7cd54ecc4eb4e31537340bb42964329
Jeffrey Clarke <jclarke@hamilton.edu>
2019-01-12 17:57:23 -0500

    writing to files

--- demo.js
+++ demo.js
@@ -1,4 +1,8 @@
 var fs = require("fs");
-fs.readdir("./newfolder", (err, data) => {
-  console.log(data);
+
+var data = {
+  name: "Bob"
+};
+fs.writeFile("data.json", JSON.stringify(data), err => {
+  console.log("write finished", err);
 });
