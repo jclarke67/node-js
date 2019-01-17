43277835c9ffd43d2faaa986d9e89b41247e4bb1
Jeffrey Clarke <jclarke@hamilton.edu>
2019-01-16 20:58:27 -0500

    messaging working

--- project/server.js
+++ project/server.js
@@ -1,6 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var messages = [
   { name: "Tim", message: "Hi" },
@@ -10,6 +16,17 @@ var messages = [
 app.get("/messages", (req, res) => {
   res.send(messages);
 });
-var server = app.listen(3000, () => {
+
+app.post("/messages", (req, res) => {
+  // console.log(req.body);
+  messages.push(req.body);
+  res.sendStatus(200);
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
+});
+
+var server = http.listen(3000, () => {
   console.log("server is listening on port", server.address().port);
 });
